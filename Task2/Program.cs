533386afa53259ffbe9fd1e29d776df0d08d00a6
Elizaveta Sidorovskaya <lizasid74@gmail.com>
2022-10-18 00:46:01 +0300

    saving

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,6 +2,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+//rjvvtyn
 Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
